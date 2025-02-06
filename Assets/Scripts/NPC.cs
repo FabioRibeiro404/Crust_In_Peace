ff77a80eb2ea4a523461f65b12be96b33c43dd09
FabioRibeiro404 <fabioribeiro.info@gmail.com>
2025-02-06 17:03:23 +0000

    Add NPCS with AI

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Profiling;
 
 public class NPC : MonoBehaviour
 {
+    [SerializeField] private Transform balcony;
+    private NPC_Order npcOrder;
+    private NavMeshAgent agent;
 
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        npcOrder = GetComponent<NPC_Order>();
+        agent.SetDestination(balcony.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Trigger Order")
+            npcOrder.SelectRandomRecipe();
+    }
+    private void UpdateDestination()
+    {
+        agent.SetDestination(balcony.position);
+    }
 }
--- Assets/Scripts/NPC_Order.cs
+++ Assets/Scripts/NPC_Order.cs
@@ -6,12 +6,6 @@ public class NPC_Order : MonoBehaviour
     [SerializeField] private List<RecipesData> _allRecipes;
     private RecipesData _choosedOrder;
 
-    private void Start()
-    {
-        SelectRandomRecipe(); 
-        ShowSelectedRecipe();
-    }
-
     public void SelectRandomRecipe()
     {
         if (_allRecipes.Count > 0)
@@ -19,12 +13,13 @@ public class NPC_Order : MonoBehaviour
             int randomIndex = Random.Range(0, _allRecipes.Count);
             _choosedOrder = _allRecipes[randomIndex];
             Debug.Log("Receita escolhida: " + _choosedOrder.recipeName);
+            ShowSelectedRecipe();
         }
         else
             Debug.LogError("Nenhuma receita disponível!");
     }
 
-    public void ShowSelectedRecipe()
+    private void ShowSelectedRecipe()
     {
         if (_choosedOrder != null)
         {
@@ -35,4 +30,9 @@ public class NPC_Order : MonoBehaviour
         else
             Debug.LogError("Nenhuma receita foi selecionada.");
     }
+
+    /*private void CheckIfOrderCorrect()
+    {
+
+    }*/
 }
