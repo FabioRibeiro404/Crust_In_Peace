dd2df4eb499084592b4257b8004082194e127bf5
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2025-02-10 18:21:58 +0000

    Add new recipes and Script and sounds to NPCs

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,13 +8,31 @@ public class NPC : MonoBehaviour
     private NavMeshAgent agent;
     private NPCOrder npcOrder;
     private Animator animator;
+    private AudioSource audioSource;
+
+    [Header("Configuração de Sons")]
+    public List<AudioClip> sounds;
+    public float minInterval = 2f; 
+    public float maxInterval = 5f;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         npcOrder = GetComponent<NPCOrder>();
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
+
+    private void Start()
+    {
+        StartCoroutine(PlayRandomSound());
+    }
+
     private void Update()
     {
         if (agent != null && animator != null)
@@ -25,5 +44,21 @@ public class NPC : MonoBehaviour
         if (agent == null || balcony == null) return;
         agent.SetDestination(balcony.position);
         npcOrder.SelectRandomRecipe();
+    }
+
+    private IEnumerator PlayRandomSound()
+    {
+        while (true)
+        {
+            if (sounds.Count > 0 && audioSource != null)
+            {
+                AudioClip clip = sounds[Random.Range(0, sounds.Count)];
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+
+            float waitTime = Random.Range(minInterval, maxInterval);
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 }
