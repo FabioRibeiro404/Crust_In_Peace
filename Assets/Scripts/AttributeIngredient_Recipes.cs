b8a8b1d71bfe940fc458c24e7fff098da6dcf0e5
FabioRibeiro404 <fabioribeiro.info@gmail.com>
2025-02-10 17:04:51 +0000

    Update Ingredients, update mechanics, update truck geometry, update lights

--- Assets/Scripts/AttributeIngredient_Recipes.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class AttributeIngredient_Recipes : MonoBehaviour
-{
-    [SerializeField] private IngredientsData ingredientsData;
-    [SerializeField] private RecipesData recipesData;
-}
---
+++ Assets/Scripts/InstantiateIngredients.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class XRInstantiateGrabbableObject : XRBaseInteractable
+{
+    [SerializeField] private GameObject grabbableObject; // Objeto a ser instanciado
+    [SerializeField] private Transform transformToInstantiate; // Posição inicial da instância
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        // Obtém o interactor (mão que interagiu com o objeto)
+        IXRSelectInteractor interactor = args.interactorObject as IXRSelectInteractor;
+
+        if (grabbableObject != null && interactor != null)
+        {
+            // Instancia o objeto na posição da mão que interagiu
+            GameObject newObject = Instantiate(grabbableObject, interactor.transform.position, interactor.transform.rotation);
+
+            // Certifica-se de que o objeto tem um XRGrabInteractable
+            XRGrabInteractable objectInteractable = newObject.GetComponent<XRGrabInteractable>();
+            if (objectInteractable == null)
+            {
+                objectInteractable = newObject.AddComponent<XRGrabInteractable>();
+            }
+
+            // Faz com que o objeto siga a mão sem ser filho
+            objectInteractable.attachTransform = interactor.GetAttachTransform(null);
+
+            // Faz com que o interactor (mão) automaticamente segure o objeto instanciado
+            interactionManager.SelectEnter(interactor, objectInteractable);
+        }
+
+        base.OnSelectEntered(args);
+    }
+}
