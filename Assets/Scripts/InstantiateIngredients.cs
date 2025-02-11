304b2961a11702293b9de3ab790468c54c9c9c6f
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2025-02-11 18:20:01 +0000

    Update amount of ingedients of the same in the world

--- Assets/Scripts/InstantiateIngredients.cs
+++ Assets/Scripts/InstantiateIngredients.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections.Generic;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
@@ -7,35 +8,38 @@ public class XRInstantiateGrabbableObject : XRBaseInteractable
 {
     [SerializeField] private GameObject grabbableObject; // Objeto a ser instanciado
     [SerializeField] private Transform transformToInstantiate; // Posição inicial da instância
-    private int maxIngredient = 5;
-    private int countIngredient;
+    private const int maxIngredient = 5;
+    private Queue<GameObject> instantiatedObjects = new Queue<GameObject>();
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        // Obtém o interactor (mão que interagiu com o objeto)
         IXRSelectInteractor interactor = args.interactorObject as IXRSelectInteractor;
 
         if (grabbableObject != null && interactor != null)
         {
-            if (countIngredient > 5)
-            { 
-                // Instancia o objeto na posição da mão que interagiu
-                GameObject newObject = Instantiate(grabbableObject, interactor.transform.position, interactor.transform.rotation);
-
-                // Certifica-se de que o objeto tem um XRGrabInteractable
-                XRGrabInteractable objectInteractable = newObject.GetComponent<XRGrabInteractable>();
-                if (objectInteractable == null)
+            // Se o número máximo foi atingido, remove o mais antigo
+            if (instantiatedObjects.Count >= maxIngredient)
+            {
+                GameObject oldestObject = instantiatedObjects.Dequeue();
+                if (oldestObject != null)
                 {
-                    objectInteractable = newObject.AddComponent<XRGrabInteractable>();
+                    Destroy(oldestObject);
                 }
-
-                // Faz com que o objeto siga a mão sem ser filho
-                objectInteractable.attachTransform = interactor.GetAttachTransform(null);
-
-                // Faz com que o interactor (mão) automaticamente segure o objeto instanciado
-                interactionManager.SelectEnter(interactor, objectInteractable);
-                countIngredient++;
             }
+
+            // Instancia novo objeto e adiciona à fila
+            GameObject newObject = Instantiate(grabbableObject, interactor.transform.position, interactor.transform.rotation);
+            instantiatedObjects.Enqueue(newObject);
+
+            // Configura interatividade XR
+            XRGrabInteractable objectInteractable = newObject.GetComponent<XRGrabInteractable>();
+            if (objectInteractable == null)
+            {
+                objectInteractable = newObject.AddComponent<XRGrabInteractable>();
+            }
+
+            objectInteractable.attachTransform = interactor.GetAttachTransform(null);
+            interactionManager.SelectEnter(interactor, objectInteractable);
         }
 
         base.OnSelectEntered(args);
