4d67d9e0041f0b2ddc574712852b5baaf292bb64
FabioRibeiro404 <fabioribeiro.info@gmail.com>
2025-02-12 17:58:05 +0000

    Add all ingredients to the pizza

--- Assets/Scripts/AttributeRecipe.cs
+++ Assets/Scripts/AttributeRecipe.cs
@@ -1,12 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AttributeRecipe : MonoBehaviour
 {
-    // Atribuir automaticamente qual é a pizza feita
-    [SerializeField] public RecipesData _recipe;
+    [SerializeField] private List<RecipesData> allRecipes;
+    public RecipesData _recipe;
 
-    private void CheckIngredients()
+    public void CheckIngredients()
     {
+        List<string> activeIngredients = new List<string>();
 
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                activeIngredients.Add(child.gameObject.name);
+            }
+        }
+
+        _recipe = FindMatchingRecipe(activeIngredients);
+
+        if (_recipe != null)
+        {
+            Debug.Log("Receita Identificada: " + _recipe.recipeName);
+        }
+        else
+        {
+            Debug.Log("Nenhuma receita correspondente encontrada.");
+        }
+    }
+
+    private RecipesData FindMatchingRecipe(List<string> activeIngredients)
+    {
+        foreach (var recipe in allRecipes)
+        {
+            List<string> recipeIngredients = new List<string>();
+
+            foreach (var ingredient in recipe.ingredients)
+            {
+                recipeIngredients.Add(ingredient.ingredients.name);
+            }
+
+            if (activeIngredients.Count == recipeIngredients.Count && !recipeIngredients.Except(activeIngredients).Any())
+            {
+                return recipe;
+            }
+        }
+        return null;
     }
 }
--- Assets/Scripts/PizzaBuilder.cs
+++ Assets/Scripts/PizzaBuilder.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class PizzaBuilder : MonoBehaviour
 {
+    private AttributeRecipe attribute;
+
+    private void Start()
+    {
+        attribute = GetComponent<AttributeRecipe>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ingredients"))
@@ -11,8 +18,27 @@ public class PizzaBuilder : MonoBehaviour
                 if (child.name.Equals("Fingers") && other.gameObject.name.Contains("Fingers"))
                     child.gameObject.SetActive(true);
 
+                if (child.name.Equals("Eyes") && other.gameObject.name.Contains("Eyes"))
+                    child.gameObject.SetActive(true);
+
+                if (child.name.Equals("Cheeses") && other.gameObject.name.Contains("Cheeses"))
+                    child.gameObject.SetActive(true);
+
+                if (child.name.Equals("Pepperonis") && other.gameObject.name.Contains("Pepperonis"))
+                    child.gameObject.SetActive(true);
+
+                if (child.name.Equals("Bugs") && other.gameObject.name.Contains("Bugs"))
+                    child.gameObject.SetActive(true);
+
+                if (child.name.Equals("Dirt") && other.gameObject.name.Contains("Dirt"))
+                    child.gameObject.SetActive(true);
+
+                if (child.name.Equals("Brains") && other.gameObject.name.Contains("Brains"))
+                    child.gameObject.SetActive(true);
+
                 Destroy(other.gameObject);
             }
+            attribute.CheckIngredients();
         }
     }
 }
